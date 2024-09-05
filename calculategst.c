ef74685e32a922d5f4633d503c6e4c5357b6d928
dipak <72122554+issc-dipak@users.noreply.github.com>
2024-09-05 21:16:37 +0530

    calcul gst

---
+++ calculategst.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void caalculat(float value);
+int main()
+{
+    float value = 100.0;
+    calculat(value);
+    return 0;
+}
+void caalculat(float value)
+{
+    value = value + (0.18 * value);
+    printf * ("final price is :%f");
+}
