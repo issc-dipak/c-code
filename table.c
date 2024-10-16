bf9606d4772579b028343a15c678a54d3dede3ee
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-15 23:58:58 -0700

    Add files via upload

--- table.c
+++ table.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main()
-{
-    int n;
-    printf("enter the n");
-    scanf("%d", &n);
-    for (int i = 10; i >= 1; i--)
-    {
-        printf("%d\n", n * i);
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
     }
+
     return 0;
 }
