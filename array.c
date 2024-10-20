6828005f7ea2d178df91636bf547352d1f9882ee
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-20 07:44:00 -0700

    array print

--- array.c
+++ array.c
@@ -1,31 +1,13 @@
-#include <stdio.h>
-
-int main() {
-    int n, i;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int arr1[n], arr2[n], sum[n];
-
-    printf("Enter elements of the first array:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr1[i]);
+#include<stdio.h>
+int main(){
+    int a[5],b;
+    printf("enter the array");
+    for(int i=0 ;i<5; i++){
+        scanf("%d",&a[i]);
     }
-
-    printf("Enter elements of the second array:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr2[i]);
+    printf("array of element");
+    for(int i=0;i<5 ; i++){
+     printf("%d",a[i]);
     }
-
-    for (i = 0; i < n; i++) {
-        sum[i] = arr1[i] + arr2[i];
-    }
-
-    printf("Sum of the two arrays:\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", sum[i]);
-    }
-
     return 0;
 }
