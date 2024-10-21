3187acbe307012f686bedb9ae6a8c85462eddad5
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-21 01:46:04 -0700

    Add files via upload

---
+++ a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the  elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("E %d \n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum of elements = %d\n", sum);
+
+    return 0;
+}
