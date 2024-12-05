3100e3fadc1cfdf681a686f79c6149ba2d913f21
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-12-05 06:13:27 -1200

    Add files via upload

--- for.c
+++ for.c
@@ -1,11 +1,8 @@
-#include<stdio.h>
-int main(){
-    int i;
-    int b = 2;
-    
-    for(i = 10; i >= 1; i--) {
-        printf("%d\n", b * i);
-    }
+#include <stdio.h>
 
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
     return 0;
 }
